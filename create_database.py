248fc275236707e42e9798e292aea16e383b7eaf
Thomas Jenny <thomas.jenny.90@gmail.com>
2024-10-20 16:25:32 +0200

    Basic table creation logic

--- create_database.py
+++ create_database.py
@@ -1,5 +1,23 @@
- #   with open("setlists.json", "w") as file:
- #       json.dump(setlists, file, indent = 4)    
+import pandas as pd
+import json
 
-# with open("releases.json", "w") as file:
-    #     json.dump(releases, file, indent = 4)
+with open("setlists.json", "r") as file:
+    setlists = json.load(file)
+
+# print(type(setlists))
+# print((setlists[0]["venue"]))
+
+venue_id, city_id, venue_name = [], [], []
+
+for i in setlists:
+    individual_venue_id = i["venue"]["id"]
+    individual_city_id = i["venue"]["city"]["id"]
+    individual_venue_name = i["venue"]["name"]
+
+    venue_id.append(individual_venue_id)
+    city_id.append(individual_city_id)
+    venue_name.append(individual_venue_name)
+
+print(len(venue_id))
+print(len(city_id))
+print(len(venue_name))

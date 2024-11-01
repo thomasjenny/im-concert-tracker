9c50180012c038d90df0141279795c819d57ea8f
Thomas Jenny <thomas.jenny.90@gmail.com>
2024-11-01 21:27:10 +0100

    Update setlists & albums join logic

--- data_prep/data_prep.py
+++ data_prep/data_prep.py
@@ -1,34 +1,77 @@
+from pathlib import Path
+
+import numpy as np
 import pandas as pd
 
 
-def join_setlists_albums(
-    setlists_df: pd.DataFrame, albums_df: pd.DataFrame
-) -> pd.DataFrame:
+# Requirements:
+# 1 file with the following structure:
+# 1 line per concert
+#   - concert_id
+#   - date
+#   - venue
+#   - city
+#   - latitude
+#   - longitude
+#   - country
+#   - tour
+#   - song_name --> all song names in 1 line, separated by <br>
+#
 
-    concerts = pd.merge(
-        setlists_df, albums_df, left_on="song_title", right_on="song_name", how="left"
-    )
 
-    concerts = concerts.drop(columns=["song_position", "song_name"])
-    return concerts
+def prepare_data_for_app(concerts: pd.DataFrame) -> pd.DataFrame:
+    pass
 
 
-if __name__ == "__main__":
-    import os
-    from pathlib import Path
 
-    in_path = Path(Path.cwd() / "data_prep" / "data" / "csv")
 
-    # Load setlists data
-    setlists_file = "setlists_clean.csv"
-    setlists = pd.read_csv(Path(in_path / setlists_file))
-    albums_file = "albums_clean.csv"
-    albums = pd.read_csv(Path(in_path / albums_file))
 
-    concerts = join_setlists_albums(setlists, albums)
 
-    # Save the clean data to CSV
-    out_path = Path(Path.cwd() / "data_prep" / "data" / "csv")
-    os.makedirs(out_path, exist_ok=True)
-    concerts.to_csv(Path(out_path / "concerts_test.csv"), index=False)
+# 1 file with all concerts per city --> if "All Tours"
+# is selected, the tooltip should show every concert
+# in the city instead of a setlist.
+#
+# 1 file with all the albums played per concert
+# for the album viz --> must enable analysis for single
+# concert and all concerts together (all tours)
+#
 
+
+
+
+# print(setlists)
+
+# Add a value to all rows where album_name is NA (i.e., non-original songs)
+# --> should not be required --> filter out all NAs in App.
+
+# Add a value to all rows where venue is an empty string
+# print(setlists.loc[setlists["venue"] == ""])
+setlists["venue"] = setlists["venue"].replace("", "Unkown Venue")
+# print(setlists.loc[setlists["venue"] == ""])
+
+
+# Convert empty strings in the cover_info column to NA
+# print(setlists)
+# print(setlists.loc[setlists["cover_info"] != "NA"])
+setlists["cover_info"] = setlists["cover_info"].replace("", "NA")
+# print(setlists.loc[setlists["cover_info"] != "NA"])
+
+print(setlists)
+# Add tape/cover info to the song names in brackets
+# song_conditions = [
+#      setlists["tape"].eq("True") & setlists["cover_info"].no
+# ]
+
+setlists.to_csv("testdata/test_setlist.csv")
+
+
+    #   mutate(
+    #     song_name = case_when(
+    #       tape == 1 & !is.na(cover_info) ~ paste0(song_name, " (from tape, oringinally by ", cover_info, ")"),
+    #       tape == 1 ~ paste0 (song_name, " (from tape"),
+    #       !is.na(cover_info) ~ paste0(song_name, " (", cover_info, " cover)"),
+    #       TRUE ~ song_name
+    #     )
+#       ) %>%
+#       
+#       # Concatenate setlist order numbers and song titles (e.g., 1. The Evil That Men Do)
---
+++ data_prep/join_setlists_albums.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+
+def join_setlists_albums(
+    setlists_df: pd.DataFrame, albums_df: pd.DataFrame
+) -> pd.DataFrame:
+    """Performs a join of the setlists and album tables to enrich the
+    setlists data with the corresponding albums to the songs
+
+    Args:
+        setlists_df (DataFrame): cleaned version of the setlist.fm
+            setlists data
+        albums_df (DataFrame): cleaned version of the musicbrainz
+            albums data
+
+    Returns:
+        concerts (DataFrame): the setlists data with an additional
+            albums column
+    """
+    concerts = pd.merge(
+        setlists_df, albums_df, left_on="song_title", right_on="song_name", how="left"
+    )
+
+    concerts = concerts.drop(columns=["song_position", "song_name"])
+    return concerts
+
+
+if __name__ == "__main__":
+    import os
+    from pathlib import Path
+
+    in_path = Path(Path.cwd() / "data_prep" / "data" / "csv")
+
+    # Load setlists data
+    setlists_file = "setlists_clean.csv"
+    setlists = pd.read_csv(Path(in_path / setlists_file))
+    albums_file = "albums_clean.csv"
+    albums = pd.read_csv(Path(in_path / albums_file))
+
+    # Run the join function
+    concerts = join_setlists_albums(setlists, albums)
+
+    # Save the clean data to CSV
+    out_path = Path(Path.cwd() / "data_prep" / "data" / "csv")
+    os.makedirs(out_path, exist_ok=True)
+    concerts.to_csv(Path(out_path / "concerts.csv"), index=False)

441d38e1a6d89eb81d4a9781952d19a8623a7e4d
Thomas Jenny <thomas.jenny.90@gmail.com>
2024-11-01 23:13:39 +0100

    Basic data prep logic for the app

--- data_prep/data_prep.py
+++ data_prep/data_prep.py
@@ -1,77 +1,72 @@
-from pathlib import Path
-
 import numpy as np
 import pandas as pd
 
 
-# Requirements:
-# 1 file with the following structure:
-# 1 line per concert
-#   - concert_id
-#   - date
-#   - venue
-#   - city
-#   - latitude
-#   - longitude
-#   - country
-#   - tour
-#   - song_name --> all song names in 1 line, separated by <br>
-#
+def prepare_setlists(concerts: pd.DataFrame) -> pd.DataFrame:
+
+    # Add a value to all rows where venue is an empty string
+    concerts["venue"] = concerts["venue"].fillna("Unknown Venue")
+    
+    # Convert empty strings in the cover_info column to NA
+    # -------> The values are already NaN
+
+    print(concerts.dtypes)
+
+    # Add tape/cover info to the song names in brackets
+    condlist = [
+         concerts["from_tape"].eq(True) & concerts["cover"].notna(),
+         concerts["from_tape"].eq(True),
+         concerts["cover"].notna()
+    ]
+
+    choicelist = [
+        np.where(
+            concerts["cover"].notna(),
+            concerts["song_title"] + " (from tape, " + concerts["cover"] + " song)",
+            concerts["song_title"]
+        ),
+        concerts["song_title"] + " (from tape)",
+        np.where(concerts["cover"].notna(),
+                 concerts["song_title"] + " (" + concerts['cover'] + " cover)",
+                 concerts["song_title"]
+        )
+    ]
+    
+    concerts["song_title"] = np.select(condlist, choicelist, default=concerts["song_title"])
 
 
-def prepare_data_for_app(concerts: pd.DataFrame) -> pd.DataFrame:
-    pass
+    # Concatenate setlist order numbers and song titles (e.g., 1. The Evil That Men Do)
+    # Loop through each concert (by unique concert index)
+    # Replace all encore numbers, but keep the first one
+    # put song titles in a list. if encore = 1, 2, etc., add this in between
+    # re-create the dataframe
 
-
-
-
+    return concerts
 
 
 # 1 file with all concerts per city --> if "All Tours"
 # is selected, the tooltip should show every concert
 # in the city instead of a setlist.
+
+def concerts_per_city():
+    pass
+
 #
 # 1 file with all the albums played per concert
 # for the album viz --> must enable analysis for single
 # concert and all concerts together (all tours)
 #
+def albums():
+    pass
 
 
+if __name__ == "__main__":
+    from pathlib import Path
 
+    path = Path(Path.cwd() / "data_prep" / "data" / "csv")
+    concerts_file = "concerts.csv"
+    concerts = pd.read_csv(Path(path / concerts_file))
 
-# print(setlists)
-
-# Add a value to all rows where album_name is NA (i.e., non-original songs)
-# --> should not be required --> filter out all NAs in App.
-
-# Add a value to all rows where venue is an empty string
-# print(setlists.loc[setlists["venue"] == ""])
-setlists["venue"] = setlists["venue"].replace("", "Unkown Venue")
-# print(setlists.loc[setlists["venue"] == ""])
-
-
-# Convert empty strings in the cover_info column to NA
-# print(setlists)
-# print(setlists.loc[setlists["cover_info"] != "NA"])
-setlists["cover_info"] = setlists["cover_info"].replace("", "NA")
-# print(setlists.loc[setlists["cover_info"] != "NA"])
-
-print(setlists)
-# Add tape/cover info to the song names in brackets
-# song_conditions = [
-#      setlists["tape"].eq("True") & setlists["cover_info"].no
-# ]
-
-setlists.to_csv("testdata/test_setlist.csv")
-
-
-    #   mutate(
-    #     song_name = case_when(
-    #       tape == 1 & !is.na(cover_info) ~ paste0(song_name, " (from tape, oringinally by ", cover_info, ")"),
-    #       tape == 1 ~ paste0 (song_name, " (from tape"),
-    #       !is.na(cover_info) ~ paste0(song_name, " (", cover_info, " cover)"),
-    #       TRUE ~ song_name
-    #     )
-#       ) %>%
-#       
-#       # Concatenate setlist order numbers and song titles (e.g., 1. The Evil That Men Do)
+    concerts = prepare_setlists(concerts)
+    
+    concerts.to_csv(Path(path / "app_test.csv"), index=False)
